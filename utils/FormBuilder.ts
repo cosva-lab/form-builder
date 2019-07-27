961e85d84d55728533c03d083da1983913e40c2b
andres <andrescastellanos112013@gmail.com>
2019-07-26 19:26:09 -0500

    The FormBuilder now has more options and allows you to have better control of the Component states.

--- utils/FormBuilder.ts
+++ utils/FormBuilder.ts
@@ -3,12 +3,14 @@ import {
   FieldsRenderBasic,
   FieldsRenderProps,
   EventField,
+  PropsField,
 } from './../index.d';
 import InputsValidator from './validate/InputsValidator';
 import transformFields from './transformFields';
 import { Component, State } from '.';
 import { changeValueFields } from './changeValues';
 
+declare type Callback = () => void;
 export default class FormBuilder extends InputsValidator
   implements FieldsRenderBasic {
   id?: number;
@@ -17,7 +19,9 @@ export default class FormBuilder extends InputsValidator
   validationState?: boolean;
   validate?: boolean;
   component?: Component;
-  setState = true;
+  originalParams: FormBuilder;
+  parmsLast?: FormBuilder;
+  changeStateComponent: boolean;
 
   constructor({
     id,
@@ -26,14 +30,74 @@ export default class FormBuilder extends InputsValidator
     validationState,
     validate,
     fields,
-  }: FieldsRenderProps) {
+    changeStateComponent = false,
+  }: FieldsRenderProps & { changeStateComponent?: boolean }) {
     super(transformFields(fields));
+    this.setProps({
+      id,
+      ns,
+      isNew,
+      validationState,
+      validate,
+    });
+    this.changeStateComponent = changeStateComponent;
+    this.originalParams = { ...this };
+  }
+
+  setProps: (
+    props: Pick<
+      FormBuilder,
+      'id' | 'ns' | 'isNew' | 'validationState' | 'validate'
+    >,
+  ) => void = ({ id, ns, isNew, validationState, validate }) => {
     this.id = id;
     this.ns = ns;
     this.isNew = isNew;
     this.validationState = validationState;
     this.validate = validate;
-  }
+  };
+
+  restoreLast = () => {
+    if (this.parmsLast) {
+      const { fields, ...rest } = this.parmsLast;
+      this.setProps(rest);
+      this.fields = fields;
+      this.parmsLast = undefined;
+    }
+  };
+
+  restore = () => {
+    const { fields, ...rest } = this.originalParams;
+    this.setProps(rest);
+    this.fields = fields;
+  };
+
+  setNew = (value: boolean, callback?: Callback) => {
+    if (this.isNew !== value) this.parmsLast = { ...this };
+    this.isNew = value;
+    if (this.component) {
+      this.component.setState(
+        state =>
+          produce<State, State>(state, (draft): void => {
+            draft.fieldsRender.isNew = value;
+          }),
+        callback,
+      );
+    }
+  };
+
+  setFields = (fields: PropsField[], callback?: Callback) => {
+    if (this.component) {
+      this.component.setState(
+        state =>
+          produce<State, State>(state, (draft): void => {
+            draft.fieldsRender.fields = fields;
+            this.fields = fields;
+          }),
+        callback,
+      );
+    }
+  };
 
   getFieldsObject = () => {
     const fields: { [key: string]: any } = {};
@@ -43,20 +107,14 @@ export default class FormBuilder extends InputsValidator
     return fields;
   };
 
-  changeField = (callback?: () => void) => ({
-    target,
-  }: EventField) => {
+  changeField = (callback?: Callback) => ({ target }: EventField) => {
     const { value, name } = target;
-    if (this.component && this.setState) {
-      this.component.setState(
-        state =>
-          produce<State, State>(state, (draft): void => {
-            const fields = draft.fieldsRender.fields;
-            draft.fieldsRender.fields = changeValueFields({
-              fields: fields,
-              action: { name, value },
-            });
-          }),
+    if (this.component && this.changeStateComponent) {
+      this.setFields(
+        changeValueFields({
+          fields: this.fields,
+          action: { name, value },
+        }),
         callback,
       );
     } else {
@@ -67,29 +125,18 @@ export default class FormBuilder extends InputsValidator
     }
   };
 
-  changeFields = (callback?: () => void) => (
-    fields: EventField[],
-  ) => {
-    const setState = this.setState;
-    this.setState = false;
+  changeFields = (callback?: Callback) => (fields: EventField[]) => {
+    const setState = this.changeStateComponent;
+    this.changeStateComponent = false;
     fields.forEach(field => {
       this.changeField()(field);
     });
-    this.setState = setState;
-
-    if (this.component) {
-      this.component.setState(
-        state =>
-          produce<State, State>(state, (draft): void => {
-            draft.fieldsRender.fields = this.fields;
-          }),
-        callback,
-      );
-    }
+    this.changeStateComponent = setState;
+    this.setFields(this.fields, callback);
   };
 
-  setValidation = (validate: boolean, callback?: () => void) => {
-    if (this.component) {
+  setValidation = (validate: boolean, callback?: Callback) => {
+    if (this.component && this.changeStateComponent) {
       this.component.setState(
         state =>
           produce<State, State>(state, (draft): void => {
