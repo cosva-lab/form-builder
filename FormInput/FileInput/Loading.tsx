6d75acbffa1d013ea1076b457f9a0b13c78b984d
andrescastellanos112013 <andrescastellanos112013@gmail.com>
2019-09-23 15:19:40 -0500

    Style problems are corrected when in dark mode.

--- FormInput/FileInput/Loading.tsx
+++ FormInput/FileInput/Loading.tsx
@@ -2,27 +2,28 @@ import React from 'react';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import makeStyles from '@material-ui/styles/makeStyles';
 import createStyles from '@material-ui/core/styles/createStyles';
+import { Theme, fade } from '@material-ui/core/styles';
 
-const styles = createStyles({
-  progress: {
-    width: '100%',
-    position: 'absolute',
-    background: '#ffffff80',
-    zIndex: 9,
-    height: '100%',
-    overflow: 'hidden',
-    bottom: 0,
-    top: 0,
-    left: 0,
-    right: 0,
-    display: 'flex',
-    justifyContent: 'center',
-    alignContent: 'center',
-    alignItems: 'center',
-  },
-});
-
-const useStyles = makeStyles(styles);
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    progress: {
+      width: '100%',
+      position: 'absolute',
+      background: fade(theme.palette.background.default, 0.7),
+      zIndex: 9,
+      height: '100%',
+      overflow: 'hidden',
+      bottom: 0,
+      top: 0,
+      left: 0,
+      right: 0,
+      display: 'flex',
+      justifyContent: 'center',
+      alignContent: 'center',
+      alignItems: 'center',
+    },
+  }),
+);
 export const Loading = () => {
   const [progress, setProgress] = React.useState(0);
   const classes = useStyles();
