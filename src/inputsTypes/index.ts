8ae97dd379182cd86bf2514e7f27114ca99ccd69
andrescastellanos112013 <andrescastellanos112013@gmail.com>
2020-01-30 01:14:26 -0500

    Components removed

--- src/inputsTypes/index.ts
+++ src/inputsTypes/index.ts
@@ -1,7 +1,4 @@
 export { Input } from './Input';
-export { DropdownList } from './DropdownList';
 export { FileInput } from './FileInput';
-export { ListSwitch } from './ListSwitch';
-export { Chips } from './Chips';
 
 export type FileValue = import('./FileInput/Props').FileValue;
