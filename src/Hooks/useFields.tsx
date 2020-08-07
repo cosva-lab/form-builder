40562e47835adea0b198df3424256e463e213e52
Eduard Castellanos <andrescastellanos112013@gmail.com>
2020-08-07 10:36:47 -0500

    [hook/useFields] It has been improved

--- src/Hooks/useFields.tsx
+++ src/Hooks/useFields.tsx
@@ -3,8 +3,13 @@ import { useLocalStore } from 'mobx-react';
 import { FieldsProps } from '../types';
 import { FieldsBuilder } from '../utils';
 
-export const useFields = (props: FieldsProps) => {
-  const field = useLocalStore(() => new FieldsBuilder(props));
-  return field;
+export const useFields = (
+  props: FieldsProps | (() => FieldsProps),
+) => {
+  const fields = useLocalStore(() => {
+    const fieldsProps = typeof props === 'function' ? props() : props;
+    return new FieldsBuilder(fieldsProps);
+  });
+  return fields;
 };
 export default useFields;
