c692123f4f637212d6a555bd83c2a576dbe18f40
Eduard Castellanos <andrescastellanos112013@gmail.com>
2020-10-14 08:07:56 -0500

    unused imports are removed

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,6 @@
 import commonjs from 'rollup-plugin-commonjs';
 import external from 'rollup-plugin-peer-deps-external';
 import resolve from 'rollup-plugin-node-resolve';
-import { terser as minify } from 'rollup-plugin-terser';
 
 export default [
   {
