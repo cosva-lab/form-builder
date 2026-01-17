27bad0c85f3c9aaf37fa66aa559f73c9f48c3393
Eduard Castellanos <andrescastellanos112013@gmail.com>
2026-01-16 21:16:59 -0500

    refactor: enhance type definitions in useField to improve type safety and flexibility

--- src/hooks/useField.tsx
+++ src/hooks/useField.tsx
@@ -1,17 +1,45 @@
 import { useLocalObservable } from 'mobx-react';
 
-import type { PropsField } from '../types';
+import type {
+  PropsField,
+  NameField,
+  FieldType,
+  CommonValidations,
+} from '../types';
 import { FieldBuilder } from '../utils/builders/FieldBuilder';
 
-export function useField<Field extends PropsField>(
-  props: PropsField<Field> | (() => PropsField<Field>),
+export function useField<
+  const V,
+  const Name extends NameField,
+  Validations extends CommonValidations | undefined = undefined,
+>(
+  props: () => PropsField<FieldType<Name, V>, Validations>,
+): FieldBuilder<FieldType<Name, V>, Validations>;
+
+export function useField<
+  const V,
+  const Name extends NameField,
+  Validations extends CommonValidations | undefined = undefined,
+>(
+  props: PropsField<FieldType<Name, V>, Validations>,
+): FieldBuilder<FieldType<Name, V>, Validations>;
+
+export function useField<
+  const V,
+  const Name extends NameField,
+  Validations extends CommonValidations | undefined = undefined,
+>(
+  props:
+    | PropsField<FieldType<Name, V>, Validations>
+    | (() => PropsField<FieldType<Name, V>, Validations>),
 ) {
   const field = useLocalObservable(() => {
     const fieldProps = typeof props === 'function' ? props() : props;
-    return new FieldBuilder<Field>(
-      fieldProps as Field & PropsField<Field>,
+    return new FieldBuilder<FieldType<Name, V>, Validations>(
+      fieldProps,
     );
   });
   return field;
 }
+
 export default useField;
